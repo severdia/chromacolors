e735e628be4c8fde35b0bec7abeef55f11e3dc56
Timothy Whitehead <twhiteheadzm@gmail.com>
2021-12-10 09:13:45 +0200

    Start/middle/end menu Clear button Color picker

--- chromacolors.js
+++ chromacolors.js
@@ -1,21 +1,31 @@
-document.addEventListener('DOMContentLoaded', function() {
-	var btnGenerateColorScheme = document.querySelector('#generate-color-scheme'),
-		btnGenerateColorScale = document.querySelector('#generate-color-scale'),
-		colorScheme = document.querySelector('.color-scheme'),
-		colorScale = document.querySelector('.color-scale'),
-		steps =  5, // is preferably an odd number
-		chromaColor, userColor, colorList;
+const STEPS = 5; // is preferably an odd number
+const USER_COLOR = '#0161b2';
 
-	btnGenerateColorScheme.addEventListener('click', function() {
+document.addEventListener('DOMContentLoaded', function () {
+	var btnGenerateColorScheme = document.getElementById('generate-color-scheme'),
+		btnGenerateColorScale = document.getElementById('generate-color-scale'),
+		colorScheme = document.querySelector('.color-scheme'),
+		userColorInput = document.getElementById('user-color'),
+		userColorPicker = document.getElementById('user-color-picker'),
+		position = document.getElementById('color-position'),
+		btnClear = document.getElementById('clear'),
+		colorScale = document.querySelector('.color-scale'),
+		stepsInput = document.getElementById('user-steps'),
+		chromaColor, colorList;
+
+	userColorInput.value = USER_COLOR;
+	stepsInput.value = STEPS;
+
+	btnGenerateColorScheme.addEventListener('click', function () {
 		// Reinitialize the color list.
 		colorList = [];
 
 		// Get the steps count
-		steps =  document.querySelector('#user-steps').value;
-	
+		steps = stepsInput.value;
+
 		// Get the color value.
-		userColor = document.querySelector('#user-color').value;
-	
+		userColor = userColorInput.value;
+
 		// Reset the content of the color scheme list.
 		colorScheme.innerHTML = '';
 
@@ -23,8 +33,22 @@ document.addEventListener('DOMContentLoaded', function() {
 		chromaColor = chroma(userColor);
 
 		// Create a monchromatic color scheme.
-		for (var i = 0; i < steps; i++) {
-			colorList[i] = chromaColor.darken(i - Math.floor(steps / 2));
+		switch (position.value) {
+			//start
+			case '1': for (var i = 0; i < steps; i++) {
+				colorList[i] = chromaColor.darken(i);
+			}
+				break;
+			//middle
+			case '2': for (var i = 0; i < steps; i++) {
+				colorList[i] = chromaColor.darken(i - Math.floor(steps / 2));
+			}
+				break;
+			//end	
+			case '3': for (var i = 0; i < steps; i++) {
+				colorList[i] = chromaColor.darken(i - (steps - 1));
+			}
+				break;
 		}
 
 		// Generate some elements.
@@ -32,13 +56,25 @@ document.addEventListener('DOMContentLoaded', function() {
 			var newItem = document.createElement('li');
 
 			newItem.style.backgroundColor = colorList[j];
-			newItem.innerHTML = '<div class="color-info">' +
-				'<span>' + colorList[j] + '</span>' +
-				'<span>' + chroma(colorList[j]).css() + '</span>' +
-				 '<span>' + chroma(colorList[j]).css('hsl') + '</span>' +
-				'</div>';
+			newItem.innerHTML = `<div class="color-info">
+									<span>${colorList[j]}</span>
+									<span>${chroma(colorList[j]).css()}</span>
+									<span>${chroma(colorList[j]).css('hsl')}</span>
+								</div>`;
 
 			colorScheme.appendChild(newItem);
 		}
+	});
+
+	btnClear.addEventListener('click', function () {
+		colorScheme.innerHTML = '';
+	});
+
+	userColorInput.addEventListener('input', function () {
+		userColorPicker.value = userColorInput.value;
+	});
+
+	userColorPicker.addEventListener('input', function () {
+		userColorInput.value = userColorPicker.value;
 	});
 });
